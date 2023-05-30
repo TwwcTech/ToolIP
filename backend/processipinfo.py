85de6693e9d274c4b21f2b82e2ecc11ed93aa2d1
twwct <twwc.tech@gmail.com>
2023-05-29 23:20:23 -0600

    output display change

--- backend/processipinfo.py
+++ backend/processipinfo.py
@@ -2,6 +2,9 @@ import requests
 import pandas as pd
 from typing_extensions import LiteralString
 
+__author__ = "twwch.tech"
+__version__ = "1.0"
+
 
 class ProcessIP:
     def __init__(self, url: LiteralString) -> None:
@@ -10,13 +13,19 @@ class ProcessIP:
     def getinfo_andfomat(self) -> pd.DataFrame:
         info: dict = requests.get(url=self.url).json()
         format_frame: dict = {
-            "IP": [f"{info['ip']}"],
-            "Country": [f"{info['country']} ({info['country_iso']})"],
-            "Region": [f"{info['region_name']} ({info['region_code']})"],
-            "ZipCode": [f"{info['zip_code']} ({info['city']})"],
-            "Coordinates": [f"{info['latitude']}, {info['longitude']}"],
-            "TimeZone": [f"{info['time_zone']}"],
-            "ISP": [f"{info['asn_org']}"]
+            "IP": f"{info['ip']}",
+            "Country": f"{info['country']} ({info['country_iso']})",
+            "Region": f"{info['region_name']} ({info['region_code']})",
+            "ZipCode": f"{info['zip_code']} ({info['city']})",
+            "Coordinates": f"{info['latitude']}, {info['longitude']}",
+            "TimeZone": f"{info['time_zone']}",
+            "ISP": f"{info['asn_org']}"
         }
         pd.set_option("display.max_columns", None)
-        return(pd.DataFrame(format_frame))
+        return (pd.Series(
+            data=format_frame,
+            index=[
+                "IP", "Country", "Region", "ZipCode",
+                "Coordinates", "TimeZone", "ISP"
+            ]
+        ))
